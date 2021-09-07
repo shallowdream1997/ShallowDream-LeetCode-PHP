bbc7cfbba84f2781f20de18734f4b9343ecbd254
zhouangang <2921675386@qq.com>
2021-09-08 01:13:04 +0800

    [+] 无限重复字符的最长字串

---
+++ lengthOfLongestSubstring/Solution.php
@@ -0,0 +1,45 @@
+﻿<?php
+
+class Solution
+{
+    /***
+     * eg1: s = "abcabcbb"
+     * print: 3
+     *
+     * eg2: s = "bbbb"
+     * print: 1
+     *
+     *
+     * eg3: s = "pwwkew"
+     * print: 3
+     *
+     * eg4: s = " "
+     * print: 0
+     */
+    function lengthOfLongestSubstring($s)
+    {
+        $arr = str_split($s,1); //拆分字符串转成数组
+        $max = [];//保存可能的最大值数组
+        while (!empty($arr)){
+            $temp = [];
+            foreach ($arr as $key => $item){
+                //如果temp不存在字母，入栈
+                if (!isset($temp[$item])){
+                    $temp[$item] = 1;
+                }else{
+                    //当有字母存在temp中，已有重复，统计当前temp的大小即为字符串的长度
+                    $max[] = count($temp);
+                    break;
+                }
+            }
+            array_shift($arr);//删除数组的开头，并偏移量向右
+        }
+        return max($max);
+    }
+
+}
+
+var_export((new Solution())->lengthOfLongestSubstring("abcabcbb"));
+var_export((new Solution())->lengthOfLongestSubstring("bbbb"));
+var_export((new Solution())->lengthOfLongestSubstring("pwwkew"));
+var_export((new Solution())->lengthOfLongestSubstring(" "));
