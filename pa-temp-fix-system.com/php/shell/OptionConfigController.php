0e85f426d09c827206a2ea9f2a93e6b29364178c
zhouangang <angang.zhou@ux168.cn>
2024-08-14 18:08:16 +0800

    [*] 增加同步脚本

--- pa-temp-fix-system.com/php/shell/OptionConfigController.php
+++ pa-temp-fix-system.com/php/shell/OptionConfigController.php
@@ -12,41 +12,8 @@ class OptionConfigController
         $this->requestUtils = new RequestUtils("test");
     }
 
-    /**
-     * 同步生产环境option-val-list -> sit环境
-     */
-    public function syncOptionValListInfoToTest(){
-        $optionNameList = [
-            "campaign_salesman_index"
-        ];
-
-        $proRequestUtils = new RequestUtils("pro");
-        foreach ($optionNameList as $optionName){
-            $proInfo = $proRequestUtils->getOptionValListByName($optionName);
-            if (!empty($proInfo)){
-                $testInfo = $this->requestUtils->getOptionValListByName($optionName);
-                if (!empty($testInfo)){
-                    //
-                    $this->requestUtils->deleteOptionValListInfo($testInfo);
-                    $this->log("delete test {$optionName}");
-                    $this->requestUtils->createOptionValListInfo($proInfo);
-                    $this->log("create test {$optionName}");
-                }else{
-                    $this->requestUtils->createOptionValListInfo($proInfo);
-                    $this->log("create test {$optionName}");
-                }
-            }
-        }
 
 
-    }
-
-    public function getSystemUserNameList(){
-        $return = $this->requestUtils->returnEmployeeByCompanySequenceId();
-        foreach ($return as $userName => $info){
-            $this->log($userName.'  '.$info['cName']);
-        }
-    }
     private function log(string $string = "")
     {
         $this->log->log2($string);
@@ -54,5 +21,3 @@ class OptionConfigController
 }
 
 $p = new OptionConfigController();
-//$p->syncOptionValListInfoToTest();
-$p->getSystemUserNameList();
