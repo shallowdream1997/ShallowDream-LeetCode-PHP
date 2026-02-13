f30ca529a7af402f5787caada4cf7d41bbf69f48
zhouangang <2921675386@qq.com>
2026-02-13 16:50:45 +0800

    feat: 调整优化页面

--- pa-temp-fix-system.com/php/job/FixQdCer.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once(dirname(__FILE__) . "/../shell/SyncJobController.php");
-
-
-$curlController = new SyncJobController();
-$curlController->fixQdCertificate();
--- pa-temp-fix-system.com/php/job/RequestPortfolio.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once(dirname(__FILE__) . "/../shell/SyncJobController.php");
-
-
-$curlController = new SyncJob1Controller();
-$curlController->requestPortfolio();
